6d4334c7023c316924d7ed0962f0e1e32d34b7ed
Torma Levente <levente.torma@moonproject.io>
2026-06-04 23:52:02 +0200

    fix: user access to filament, add edit profile page

---
+++ app/Filament/Pages/Auth/EditProfile.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filament\Pages\Auth;
+
+use Filament\Auth\Pages\EditProfile as BaseEditProfile;
+use Filament\Schemas\Schema;
+
+class EditProfile extends BaseEditProfile
+{
+    public function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                $this->getNameFormComponent(),
+                $this->getEmailFormComponent(),
+                $this->getPasswordFormComponent(),
+                $this->getPasswordConfirmationFormComponent(),
+            ]);
+    }
+}
